f57ffa5130e7cb151b120107213dee0f4bdedb9d
Aditya <adityapanda004@gmail.com>
2026-08-27 01:24:49 +0530

    fix: allow production frontend cors

--- server/src/app.js
+++ server/src/app.js
@@ -6,7 +6,8 @@ const app = express();
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        "http://localhost:3000"
+        "http://localhost:3000",
+        "https://apishield-theta.vercel.app"
     ],
     credentials: true
 }));
